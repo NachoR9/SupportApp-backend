a101d82661c5bc6c4af5faba6064a09aa37dd9b0
NachoR9 <nachorm9@hotmail.com>
2024-07-08 14:14:52 +0200

    project spring boot

---
+++ src/test/java/dev/nacho/supportApp/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nacho.supportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
