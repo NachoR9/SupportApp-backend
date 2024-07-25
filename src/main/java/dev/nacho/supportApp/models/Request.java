28ac79c5daf98fce867e9bf6be9e231055f6c06d
NachoR9 <nachorm9@hotmail.com>
2024-07-25 15:34:32 -0700

    Request class done

--- src/main/java/dev/nacho/supportApp/models/Request.java
+++ src/main/java/dev/nacho/supportApp/models/Request.java
@@ -1,5 +1,7 @@
 package dev.nacho.supportApp.models;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,25 +15,65 @@ public class Request {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_request")
+    @Column(name = "id")    
     private Long id;
 
+    @Column(name = "name")
     private String name;
+    @Column(name = "date")
+    private LocalDateTime date;
+    @Column(name = "topic")
+    private String topic;
+    @Column(name = "description")
+    private String description;
 
     public Request() {
     }
 
-    public Request(Long id, String name) {
+    public Request(Long id, String name, LocalDateTime date, String topic, String description) {
         this.id = id;
         this.name = name;
+        this.date = date;
+        this.topic = topic;
+        this.description = description;
     }
-
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
